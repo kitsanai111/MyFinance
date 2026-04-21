5da3df859d17daf4886179ab7ed27fe7f73e428b
kitsanai111 <90263867+kitsanai111@users.noreply.github.com>
2026-04-21 12:55:21 +0700

    server.js

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ readdirSync('./routes').forEach(file => {
   }
 });
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
-});
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
