fc94d2e46273ca143b263e18a2ab811e546fae1a
kritsanai <kitsuya70@gmail.com>
2026-05-07 22:11:21 +0700

    update api url

--- client/src/config.js
+++ client/src/config.js
@@ -11,7 +11,7 @@
 // export const BASE_API_URL = "http://172.21.12.33:5001/api"
 
 //Radmin
-export const BASE_API_URL = "http://26.248.21.114:5001/api"
+export const BASE_API_URL = "https://myfinance-bpbb.onrender.com/api"
 
 
 // export const BASE_API_URL = "http://10.50.160.226:5001/api"
